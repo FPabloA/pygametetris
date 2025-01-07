0ff4aa44179b274f34910370b1f84272df3be97e
Pablo Ayala <pabloayala@protonmail.com>
2025-01-07 11:59:58 -0600

    added code for falling pieces, clearing rows, drawing grid, etc

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 
+import pygame.docs
+
 #tetris code being adapted from https://www.techwithtim.net/tutorials/game-development-with-python/tetris-pygame/tutorial-1
 
 pygame.init()
@@ -170,8 +172,14 @@ class TetrisGame():
         global shapes, shape_colors
         return Piece(5, 0, random.choice(shapes))
     
-    def drawGrid(self, surface, row, col):
-        pass
+    def drawGrid(self, row, col):
+        sx = self.top_left_x
+        sy = self.top_left_y
+        for i in range(row):
+            pygame.draw.line(self.display, (128,128,128), (sx, sy + i * 30), (sx + self.w, sy + i * 30))
+            
+            for j in range(col):
+                pygame.draw.line(self.display, (128,128,128), (sx + j * 30, sy), (sx + j * 30, sy + self.h))
 
     
     def playStep(self):
@@ -186,6 +194,21 @@ class TetrisGame():
         fall_time = 0
 
         while run:
+
+            fall_speed = 0.27
+
+            self.grid = self.createGrid(locked_positions)
+            fall_time += self.clock.get_rawtime()
+            self.clock.tick()
+
+            #piece falling
+            if fall_time/1000 >= fall_speed:
+                fall_time = 0
+                currPiece.y += 1
+                if not(self.validSpace(currPiece)) and currPiece.y > 0:
+                    currPiece.y -= 1
+                    changePiece = True
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
@@ -195,26 +218,47 @@ class TetrisGame():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
                         currPiece.x -= 1
-                        if not self.validSpace(currPiece, self.grid):
+                        if not self.validSpace(currPiece):
                             currPiece.x += 1
                     
                     elif event.key == pygame.K_RIGHT:
                         currPiece.x += 1
-                        if not self.validSpace(currPiece, self.grid):
+                        if not self.validSpace(currPiece):
                             currPiece.x -= 1
 
                     elif event.key == pygame.K_UP:
                         #rotate shape
                         currPiece.rotation = currPiece.rotation + 1 % len(currPiece.shape)
-                        if not self.validSpace(currPiece, self.grid):
+                        if not self.validSpace(currPiece):
                             currPiece.rotation = currPiece.rotation - 1 % len(currPiece.shape)
                     
                     elif event.key == pygame.K_DOWN:
                         currPiece.y += 1
-                        if not self.validSpace(currPiece, self.grid):
+                        if not self.validSpace(currPiece):
                             currPiece.y -= 1
+
+            shapePos = self.convShape(currPiece)
+
+            for i in range(len(shapePos)):
+                x, y = shapePos[i]
+                if y > -1:
+                    self.grid[y][x] = currPiece.color
+            
+            if changePiece:
+                for pos in shapePos:
+                    p = (pos[0], pos[1])
+                    locked_positions[p] = currPiece.color
+                currPiece = nextPiece
+                nextPiece = self.getPiece()
+                changePiece = False
+            
+            self.clearRows(locked_positions)
+
         
-        self.drawWindow(self.display)
+            self.drawWindow(self.display)
+            self.drawNextShape(nextPiece)
+            pygame.display.update()
+
 
     def drawWindow(self, surface):
         print("in draw window")
@@ -231,16 +275,81 @@ class TetrisGame():
                 pygame.draw.rect(surface, self.grid[i][j], (self.top_left_x + j*30, self.top_left_y + i*30, 30, 30), 0)
         
         #draw grid and border
-        self.drawGrid(surface, 20, 10)
+        self.drawGrid(20, 10)
         pygame.draw.rect(surface, (255, 0, 0), (self.top_left_x, self.top_left_y, self.w, self.h), 5)
         pygame.display.update()
 
-    def validSpace(self):
-        pass
+    def validSpace(self, shape):
+        acceptedPos = [[(j, i) for j in range(10) if self.grid[i][j] == (0,0,0)] for i in range(20)]
+        acceptedPos = [j for sub in acceptedPos for j in sub]
+        formatted = self.convShape(shape)
 
-    def isTopReached(self):
-        #TODO write code for checking if game lost
-        pass
+        for pos in formatted:
+            if pos not in formatted:
+                if pos not in acceptedPos:
+                    if pos[1] > -1:
+                        return False
+
+        return True
+
+    def convShape(self, shape):
+        positions = []
+        format = shape.shape[shape.rotation % len(shape.shape)]
+
+        for i, line in enumerate(format):
+            row = list(line)
+            for j, column in enumerate(row):
+                if column == '0':
+                    positions.append((shape.x + j, shape.y + i))
+        
+        for i, pos in enumerate(positions):
+            positions[i] = (pos[0] - 2, pos[1] - 4)
+
+        return positions
+
+    def isTopReached(self, positions):
+        for pos in positions:
+            x, y = pos
+            if y < 1:
+                return True
+        return False
+
+    def drawNextShape(self, shape):
+        font = pygame.font.SysFont('comicsans', 30)
+        label = font.render('Next Shape', 1, (255,255,255))
+
+        sx =self.top_left_x + self.w + 50
+        sy = self.top_left_y + self.h/2 - 100
+
+        format = shape.shape[shape.rotation % len(shape.shape)]
+
+        for i, line in enumerate(format):
+            row = list(line)
+            for j, column in enumerate(row):
+                if column == '0':
+                    pygame.draw.rect(self.display, shape.color, (sx + j * 30, sy + i * 30, 30, 30), 0)
+        
+        self.display.blit(label, (sx + 10, sy - 30))
+    
+    def clearRows(self, locked):
+        inc = 0
+        for i in range(len(self.grid)-1, -1, -1):
+            row = self.grid[i]
+            if (0,0,0) not in row:
+                inc += 1
+
+                ind = 1
+                for j in range(len(row)):
+                    try:
+                        del locked[(i,j)]
+                    except:
+                        continue
+        if inc > 0:
+            for key in sorted(list(locked), key = lambda x: x[1])[::-1]:
+                x, y = key
+                if y < ind:
+                    newKey = (x, y + inc)
+                    locked[newKey] = locked.pop(key)  
 
     def move(self):
         #TODO write code for moving pieces left or right (maybe rotation should be in here)
